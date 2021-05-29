029d5921ec1ea4793f159bcc851112dc381f75e7
REGO350 <riokanehiro228@gmail.com>
2021-05-29 12:17:00 +0900

    minor fix

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
-const SERVER_URL = "https://odn2hgteqiok.moralis.io:2053/server";
-const APPLICATION_ID = "D0AXstFCUKQZ70du1Tg0U1zeiqMc9sJrmWkX5Abg";
-var contractAddress = "0x10CcF9f4B1982C7A9354bfEae54785045F5dBCA5";
+const SERVER_URL = "https://oxier2akjysv.moralis.io:2053/server";
+const APPLICATION_ID = "PSOHooRdsiTBOywifwEZbxgC8fIDkxrFgdhvCf9A";
+var contractAddress = "0xBB07E1F6AF04C5Aecd36ed480e4d951af5865319";
 
 Moralis.initialize(APPLICATION_ID); // Application id from moralis.io
 Moralis.serverURL = SERVER_URL; //Server url from moralis.io
 
 function addRowToTable(tableId, data){
-  let tableRow = document.createElement('tr');
-  data.forEach(element =>{
-    let newColumn = document.createElement('td');
-    newColumn.innerHTML = element;
-    tableRow.appendChild(newColumn);
+  let tableRow = $("<tr></tr>");
+  data.forEach(element => {
+    let newColumn = $("<td></td>").text(element);
+    tableRow.append(newColumn);
   });
-  document.getElementById(tableId).appendChild(tableRow);
+  $(`#${tableId}`).append(tableRow);
 }
 
 async function login() {
   try {
     user = await Moralis.User.current();
-    if(!user){
+    if (!user) {
+      console.log("authenticate");
       user = await Moralis.Web3.authenticate();
     }
     console.log(user);
-    document.getElementById("login_button").style.display = "none";
-    document.getElementById("game").style.display = "block";
-    document.getElementById("logout_button").style.display = "block";
+    $("#login_button").css("display", "none");
+    $("#logout_button").css("display", "block");
+    $("#game").css("display", "block");
 
     let biggestWinners = await Moralis.Cloud.run("biggestWinners", {});
-    biggestWinners.forEach( (row) => {
-      addRowToTable("top_winners", [row.objectId, row.total_sum]);
+    biggestWinners.forEach((row)=>{
+      addRowToTable("top_winners", [row.objectId, row.total]);
     });
 
-
     let biggestLosers = await Moralis.Cloud.run("biggestLosers", {});
-    biggestLosers.forEach( (row) => {
-      addRowToTable("top_losers", [row.objectId, row.total_sum]);
+    biggestLosers.forEach((row)=>{
+      addRowToTable("top_losers", [row.objectId, row.total]);
     });
 
     let biggestBets = await Moralis.Cloud.run("biggestBets", {});
-    biggestBets.forEach( (row) => {
+    biggestBets.forEach((row)=>{
       addRowToTable("biggest_bets", [row.user, row.bet, row.win]);
     });
 
@@ -47,28 +46,44 @@ async function login() {
   }
 }
 
-async function flip(side){
-  let amount = document.getElementById("amount").value;
-  window.web3 = await Moralis.Web3.enable();
-  let contractInstance = new web3.eth.Contract(window.abi, contractAddress);
-  contractInstance.methods.flip(side == "heads" ? 0 : 1).send({value: amount, from: ethereum.selectedAddress}).on('receipt', (receipt) =>{
-    console.log(receipt);
-    if(receipt.events.bet.returnValues.win){
-      alert("you won!");
-    }else{
-      alert("you lost!");
-    }
-  })
-}
-
 async function logout(){
   await Moralis.User.logOut();
-  document.getElementById("login_button").style.display = "block";
-  document.getElementById("logout_button").style.display = "none";
-  alert("logged out");
+  alert("Logged out");
+  $("#login_button").css("display", "block");
+  $("#logout_button").css("display", "none");
+  $("#game").css("display", "none");
+
 }
 
-document.getElementById("login_button").onclick = login;
-document.getElementById("logout_button").onclick = logout;
-document.getElementById("heads_button").onclick = () => {flip("heads")};
-document.getElementById("tails_button").onclick = () => {flip("tails")};
+async function flip(side) {
+  let amount = $("#amount").val();
+  window.web3 = await Moralis.Web3.enable();
+  let contractInstance = new web3.eth.Contract(window.abi, contractAddress);
+  contractInstance.methods
+    .flip(side == "heads" ? 0 : 1)
+    .send({ value: amount, from: ethereum.selectedAddress })
+    .on("receipt", (receipt) => {
+      console.log(receipt);
+      if (receipt.events.bet.returnValues.win) {
+        alert("you won");
+      } else {
+        alert("you lost");
+      }
+    });
+}
+
+$("#login_button").click(() => {
+  login();
+});
+
+$("#logout_button").click(() => {
+  logout();
+});
+
+$("#heads_button").click(() => {
+  flip("heads");
+});
+
+$("#tails_button").click(() => {
+  flip("tails");
+});
